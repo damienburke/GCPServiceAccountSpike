a845bb94188401ffc50c9a35b62761bdf94fd64e
Damien Burke <damienburke@beyond.com>
2025-02-21 09:40:31 +0000

    link project

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,8 @@ repositories {
 }
 
 dependencies {
+    implementation("com.google.cloud:google-iam-admin:3.53.0")
+    implementation("com.google.code.gson:gson:2.10.1")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
